36f2eb228ef0b49580c3b02632c0ceeafb798da2
Adithya K B <adithyakb93@gmail.com>
2025-07-17 21:24:39 +0530

    style: Add intel mono font and header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,4 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -13,11 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`intialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import Image from 'next/image';
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
 import {
   Carousel,
   CarouselContent,
@@ -8,66 +8,74 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import car from "/public/car.svg"
-import bus from "/public/bus.svg"
-import auto from "/public/auto.svg"
+import { Card, CardContent } from "@/components/ui/card";
+import car from "/public/car.svg";
+import bus from "/public/bus.svg";
+import auto from "/public/auto.svg";
 
 const vehicles = [
   {
     name: "auto",
-    src: auto
+    src: auto,
   },
   {
     name: "car",
-    src: car
-  },{
-    name: "bus",
-    src: bus
+    src: car,
   },
-]
+  {
+    name: "bus",
+    src: bus,
+  },
+];
 
 const Page = () => {
+  const [currentVehicle, setCurrentVehicle] = useState(vehicles[0].name);
 
-  const [currentVehicle, setCurrentVehicle] = useState(vehicles[0].name)
-
-  useEffect(()=>{
-    console.log(currentVehicle)
-  },[currentVehicle])
+  useEffect(() => {
+    console.log(currentVehicle);
+  }, [currentVehicle]);
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center bg-black">
-      <div>
-        <Carousel className="w-full max-w-xs">
-          <CarouselContent>
-            {vehicles.map((vehicle,idx) => (
-              <CarouselItem key={idx}>
-                <div className="p-1">
-                  <Card className="bg-[#171717]">
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <span className="text-4xl font-semibold text-white select-none">
-                        <Image src={vehicle.src} alt={vehicle.name} onClick={() => setCurrentVehicle(vehicle.name)}/>
-                      </span>
-                    </CardContent>
-                  </Card>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+    <div className="bg-black text-white py-20 h-screen">
+      <div className="flex flex-col text-center items-center gap-5">
+        <p className="text-7xl text-center font-medium">
+          Kerala Fare Calculator
+        </p>
+        <p className="text-2xl">
+          {" "}
+          A simpler, faster, and more accurate way to
+          <br />
+          calculate your travel fare in Kerala
+        </p>
+
+        <div className="mt-20">
+          <Carousel className="w-full max-w-xs">
+            <CarouselContent>
+              {vehicles.map((vehicle, idx) => (
+                <CarouselItem key={idx}>
+                  <div className="p-1">
+                    <Card className="bg-[#171717]">
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-4xl font-semibold text-white select-none">
+                          <Image
+                            src={vehicle.src}
+                            alt={vehicle.name}
+                            onClick={() => setCurrentVehicle(vehicle.name)}
+                          />
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Page
+export default Page;
